c74eba7665368d7de0b2c486f9bf579e5e772076
ALEXfeed1407 <100508764+ALEXfeed1407@users.noreply.github.com>
2023-04-05 15:33:59 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -10,8 +10,8 @@ def solution(p: float, x: np.array) -> tuple:
     import scipy.stats as st
     alpha = 1 - p
     S_new = sum([item**2 for item in x])
-    z1 = st.chi2.ppf(alpha,df= len(x)*2)
-    z2 = st.chi2.ppf(1- alpha,df= len(x)*2)
+    z1 = st.chi2.ppf(alpha/2,df= len(x)*2)
+    z2 = st.chi2.ppf(1- alpha/2,df= len(x)*2)
     a = np.sqrt( S_new/(z2 * 47))
     b = np.sqrt( S_new/(z1 * 47) )
     return (a, b)
