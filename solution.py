253b3ba2e119eab85d4f06414a7fcacab490a16c
ALEXfeed1407 <100508764+ALEXfeed1407@users.noreply.github.com>
2023-04-05 15:14:45 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,12 +7,11 @@ from scipy.stats import norm
 chat_id = 288759659 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    from scipy.stats import chi2
-    n = x.shape[0]
-    sample_mean = np.mean(x)
-    sample_var = np.var(x, ddof=1) / 47
+    import scipy.stats as st
     alpha = 1 - p
-    t = (n - 1) * sample_var / chi2.ppf([1 - alpha/2, alpha/2], df= n - 1)
-    ci_low = np.sqrt(t[0])    
-    ci_high = np.sqrt(t[1])
-    return ci_low, ci_high
+    S_new = sum([item**2 for item in x])
+    z1 = st.chi2.ppf(alpha,df= len(x)*2)
+    z2 = st.chi2.ppf(1- alpha,df= len(x)*2)
+    a = np.sqrt( S_new/(z2 * 47))
+    b = np.sqrt( S_new/(z1 * 47) )
+    return (a, b)
